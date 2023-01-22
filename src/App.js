ae849cea763c38b81e47632e37c7a6cdbcdd0fe9
Monika <monikameena797@gmail.com>
2023-01-22 16:45:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+//ADD TOAST NOTIFICATIONS
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+//IMPPORT COMPONENTS
+import Navbar from "./components/Navbar/Navbar";
+import WeekView from "./components/WeekView/WeekView";
+import AddHabit from "./components/AddHabit/AddHabit";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <ToastContainer position="top-right" autoClose={1000} />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/add" element={<AddHabit />}></Route>
+        <Route path="/weekview/:id" element={<WeekView />}></Route>
+      </Routes>
     </div>
   );
 }
